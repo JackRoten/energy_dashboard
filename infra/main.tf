230f43e52efcb522fab066ecc177b7282ae9d302
JackRoten <thejackroten@gmail.com>
2025-11-11 14:36:11 -0800

    Add main.py

--- infra/main.tf
+++ infra/main.tf
@@ -2,14 +2,13 @@ provider "aws" {
   region = var.region
 }
 
-# 👇 Fetch your default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
 module "rds" {
   source         = "./modules/rds"
-  db_secret_name = "api_postgres_secret_v2" # Changed to avoid conflict with deleted secret
+  db_secret_name = "api_postgres_secret_v2"
 }
 
 module "api_lambda" {
