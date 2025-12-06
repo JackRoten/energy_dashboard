6f377b451bcdcd872da8d2849b9da3067f71148e
JackRoten <thejackroten@gmail.com>
2025-12-05 23:04:10 -0800

    Add region, ecr repo, cluster, service, and task def family secrets

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -106,3 +106,33 @@ resource "github_actions_secret" "deploy_role_arn" {
   secret_name = "AWS_DEPLOY_ROLE_ARN"
   plaintext_value = aws_iam_role.github_actions_deploy_role.arn
 }
+
+resource "github_actions_secret" "aws_region" {
+  repository  = "energy_dashboard"
+  secret_name = "AWS_REGION"
+  plaintext_value = "us-west-2"
+}
+
+resource "github_actions_secret" "ecr_repository" {
+  repository  = "energy_dashboard"
+  secret_name = "ECR_REPOSITORY"
+  plaintext_value = "react-app"
+}
+
+resource "github_actions_secret" "ecs_cluster" {
+  repository  = "energy_dashboard"
+  secret_name = "ECS_CLUSTER"
+  plaintext_value = "react-app-cluster"
+}
+
+resource "github_actions_secret" "ecs_service" {
+  repository  = "energy_dashboard"
+  secret_name = "ECS_SERVICE"
+  plaintext_value = "react-app-service"
+}
+
+resource "github_actions_secret" "task_definition_family" {
+  repository  = "energy_dashboard"
+  secret_name = "TASK_DEFINITION_FAMILY"
+  plaintext_value = "react-app-task"
+}
