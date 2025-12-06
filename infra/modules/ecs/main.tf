a6c589d9a42ed042fbabe3abb02803517c274430
JackRoten <thejackroten@gmail.com>
2025-12-06 13:05:02 -0800

    Add ecs-task-role resource and output. task-definition.json added to include configuration for github actions render-task

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -49,6 +49,23 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 # --------------------------
 # Load Balancer
 # --------------------------
@@ -174,4 +191,24 @@ resource "aws_ecs_service" "react_app" {
   depends_on = [
     aws_lb_listener.http
   ]
+}
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
+
+resource "github_actions_secret" "ecs_task_execution_role_arn" {
+  repository  = "energy_dashboard"
+  secret_name = "ECS_TASK_EXECUTION_ROLE_ARN"
+  plaintext_value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+resource "github_actions_secret" "ecs_task_role_arn" {
+  repository  = "energy_dashboard"
+  secret_name = "ECS_TASK_ROLE_ARN"
+  plaintext_value = aws_iam_role.ecs_task_role.arn
 }
